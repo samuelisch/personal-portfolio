7a00329ad1b2f4977052b0092bce8311954f6d48
Samuelisch <samuelchanwk@hotmail.com>
2022-02-11 20:50:14 +0800

    added behaviour to highlight navbar text on scroll, changed element styling to fit navbar highlight conditions, added footer element

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "./sass/main.scss";
 
 const hoverTexts = document.querySelectorAll('.hover');
 const project = document.querySelectorAll('.project');
+const sections = document.querySelectorAll('section[id]');
 
 function showImg(e: Event) {
   const target:any = e.target;
@@ -37,6 +38,28 @@ function hideGif(e: Event) {
   }
 }
 
+//highlighting nav element when content is shown
+function navHighlighter() {
+  //current scroll pos
+  let scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const currentSection:any = (<Element>current)
+    const sectionHeight = currentSection.offsetHeight;
+    const sectionTop = currentSection.offsetTop - 250;
+    const sectionId = current.getAttribute('id');
+
+    //compare
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      (<Element>document.querySelector(`.nav-link[href*="${sectionId}"]`)).classList.add('current');
+    } else {
+      (<Element>document.querySelector(`.nav-link[href*="${sectionId}"]`)).classList.remove('current');
+    }
+  })
+}
+
+//listen for scroll
+window.addEventListener('scroll', navHighlighter);
 hoverTexts.forEach(text => text.addEventListener('mouseover', showImg));
 hoverTexts.forEach(text => text.addEventListener('mouseout', hideImg));
 project.forEach(project => project.addEventListener('mouseenter', hideGif));
