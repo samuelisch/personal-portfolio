176dace2e3655382350e9acea3b02782d1951c1a
Samuelisch <samuelchanwk@hotmail.com>
2022-02-11 10:07:12 +0800

    added base color for project details, components and styling for individual projects

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,6 @@
 import "./sass/main.scss";
-import Concerto from './images/concerto.png';
-import Cycle from './images/cycle.png';
-import Formula from './images/formula.png';
 
 const hoverTexts = document.querySelectorAll('.hover');
-
-const concertoElement = document.querySelector('.concerto');
-const concertoImage = new Image();
-concertoImage.className = 'concerto-img interest-img';
-concertoImage.src = Concerto;
-concertoElement?.appendChild(concertoImage);
-
-const cycleElement = document.querySelector('.cycle');
-const cycleImage = new Image();
-cycleImage.className = 'cycle-img interest-img';
-cycleImage.src = Cycle;
-cycleElement?.appendChild(cycleImage);
-
-const formulaElement = document.querySelector('.formula');
-const formulaImage = new Image();
-formulaImage.className = 'formula-img interest-img';
-formulaImage.src = Formula;
-formulaElement?.appendChild(formulaImage);
 
 function showImg(e: Event) {
   const target:any = e.target;
