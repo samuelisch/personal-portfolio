973eedf820372c3c6263a536a32064bb5a32556a
Samuelisch <samuelchanwk@hotmail.com>
2022-02-13 10:58:22 +0800

    added transitions for hero element

---
+++ src/transitions.ts
@@ -0,0 +1,17 @@
+function animateHero() {
+  const firstLine = document.querySelector('.hero-one');
+  const secondLine = document.querySelector('.hero-two');
+  const viewButton = document.querySelector('.view-button');
+  firstLine?.classList.add('animate-hero');
+  secondLine?.classList.add('animate-hero');
+  viewButton?.classList.add('animate-button');
+  
+}
+
+
+document.addEventListener('readystatechange', event => {
+  const target:any = event.target;
+  if (target.readyState === 'complete') {
+    animateHero();
+  }
+});
